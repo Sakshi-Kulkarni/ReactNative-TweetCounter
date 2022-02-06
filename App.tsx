39e0b9655fe641ed0817531188ade26ca37a702d
sakshi kulkarni <kulkarnisakshi2410@gmail.com>
2022-02-06 22:56:55 +0530

    completed

--- App.tsx
+++ App.tsx
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+ 
+const char_limit = 280;
 
-export default function App() {
+const App = () => {
+
+  const [limit, setlimit] = useState('');
+  const char_left = char_limit - limit.length;
+
+
+  let color;
+  let bgcolor;
+  if (char_left < 0) {
+    color = 'red';
+    bgcolor = 'pink';
+  } else if (char_left <= 10) {
+    color = 'yellow';
+    bgcolor = '#fcf39d';
+  }
+
+  const handleChange = (Character: string) => {
+    setlimit(Character);
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      paddingTop: 150
+    },
+    input: {
+      margin: 20,
+      padding: 15,
+      height: 290,
+      borderColor: color,
+      backgroundColor: bgcolor,
+      borderWidth: 4,
+      borderRadius: 20
+    },
+    line: {
+      textAlign: 'center',
+      fontSize: 25,
+      color: color
+    }
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        editable={true}
+        numberOfLines={5}
+        multiline={true}
+        style={styles.input}
+        onChangeText={val => handleChange(val)}
+        value={limit}
+      />
+
+      <Text style={styles.line}>{char_left}  Character Remaining </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
